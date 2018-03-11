38cba329e611aad6815194ebda1f2a72f89e0e30
Hassan Zekkouri <hassanzekkouri@gmail.com>
2018-03-11 18:10:29 +0000

    Update files

--- Forums/main.py
+++ Forums/main.py
@@ -7,3 +7,10 @@ member2 = models.Member("Mohammad", 24)
 post1 = models.Post("Classes", "OOP blueprints!")
 post2 = models.Post("Inheritance", "It makes classes for reusability!")
 post3 = models.Post("Variables", "Every code has Variables!")
+
+print("="*10 + " Members " + "="*10)
+print member1
+print member2
+print("="*10 + " Posts " + "="*10)
+print post1
+print post2
--- Forums/models.py
+++ Forums/models.py
@@ -12,3 +12,6 @@ class Post:
 	def __init__(self, title, body):
 		self.title = title
 		self.body = body
+
+	def __str__(self):
+		return ("Title: {}\nBody: {}".format(self.title, self.body))
