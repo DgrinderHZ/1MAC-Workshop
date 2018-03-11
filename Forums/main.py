9c2000d910265cb0a6b0953840ecb43921ecb89c
Hassan Zekkouri <hassanzekkouri@gmail.com>
2018-03-11 19:41:37 +0000

    Add stores

--- Forums/main.py
+++ Forums/main.py
@@ -8,9 +8,27 @@ post1 = models.Post("Classes", "OOP blueprints!")
 post2 = models.Post("Inheritance", "It makes classes for reusability!")
 post3 = models.Post("Variables", "Every code has Variables!")
 
+member_store = models.MemberStore()
+post_store = models.PostStore()
+
 print("="*10 + " Members " + "="*10)
 print member1
 print member2
 print("="*10 + " Posts " + "="*10)
 print post1
 print post2
+
+member_store.add(member1)
+member_store.add(member2)
+print("="*10 + " Members Store " + "="*10)
+print(member_store.get_all())
+
+post_store.add(post1)
+post_store.add(post2)
+post_store.add(post3)
+print("="*10 + " Posts Store " + "="*10)
+print(post_store.get_all())
+
+print(":::END:::")
+
+
--- Forums/models.py
+++ Forums/models.py
@@ -14,4 +14,26 @@ class Post:
 		self.body = body
 
 	def __str__(self):
-		return ("Title: {}\nBody: {}".format(self.title, self.body))
+		return ("Title: {}\nBody: {}\n".format(self.title, self.body))
+
+
+class MemberStore:
+	members = []
+
+	def add(self, member):
+		self.members.append(member)
+
+	def get_all(self):
+		for member in self.members:
+			print(member)
+
+
+class PostStore:
+	posts = []
+
+	def add(self, post):
+		self.posts.append(post)
+
+	def get_all(self):
+		for post in self.posts:
+			print(post)
