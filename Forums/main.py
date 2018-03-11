d36568c2cb7b3f935d913f450f3275284707a2ec
Hassan Zekkouri <hassanzekkouri@gmail.com>
2018-03-11 17:28:29 +0000

    Update codes

---
+++ Forums/main.py
@@ -0,0 +1,9 @@
+import models
+
+
+member1 = models.Member("Hassan", 22)
+member2 = models.Member("Mohammad", 24)
+
+post1 = models.Post("Classes", "OOP blueprints!")
+post2 = models.Post("Inheritance", "It makes classes for reusability!")
+post3 = models.Post("Variables", "Every code has Variables!")
