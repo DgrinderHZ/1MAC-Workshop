f5ccdeb95d60a64ea7029cb18e6ef0dfe467842e
Hassan Zekkouri <hassanzekkouri@gmail.com>
2018-03-14 22:16:27 +0000

    Update

--- Forums/stores.py
+++ Forums/stores.py
@@ -3,64 +3,63 @@ import itertools
 import copy
 
 class MemberStore:
-	members = []
-	last_id = 1
+    members = []
+    last_id = 1
 
-	def add(self, member):
-		member.id = MemberStore.last_id
-		MemberStore.members.append(member)
-		MemberStore.last_id += 1
+    def add(self, member):
+        member.id = MemberStore.last_id
+        MemberStore.members.append(member)
+        MemberStore.last_id += 1
 
-	def get_all(self):
-		return MemberStore.members
+    def get_all(self):
+        return MemberStore.members
 
-	def get_by_id(self, id):
-		all_members = self.get_all()
-		result = None
-		for member in all_members:
-			if (member.id == id):
-				result = member
-				break
+    def get_by_id(self, id):
+        all_members = self.get_all()
+        result = None
+        for member in all_members:
+            if (member.id == id):
+                result = member
+                break
 
-		return result
+        return result
 
-	def get_by_name(self, name):
-		all_members = self.get_all()
+    def get_by_name(self, name):
+        all_members = self.get_all()
 
-		for member in all_members:
-			if member.name == name:
-				yield member
+        for member in all_members:
+            if member.name == name:
+                yield member
 
-	def entity_exists(self, member):
-		result = False
+    def entity_exists(self, member):
+        result = False
 
-		if member in MemberStore.members:
-			result = True
+        if member in MemberStore.members:
+            result = True
 
-		return result
+        return result
 
-	def delete(self, id):
-		member = self.get_by_id(id)
-		if member:
-			MemberStore.members.remove(member)
+    def delete(self, id):
+        member = self.get_by_id(id)
+        if member:
+            MemberStore.members.remove(member)
 
-	def update(self, member):
-		all_members = self.get_all()
-		for index, current_member in enumerate(all_members):
-			if member.id == current_member.id:
-				MemberStore.members[index] = member
-				break
+    def update(self, member):
+        all_members = self.get_all()
+        for index, current_member in enumerate(all_members):
+            if member.id == current_member.id:
+                MemberStore.members[index] = member
+                break
 
-	def get_members_with_posts(self, all_posts):
-		all_members = copy.deepcopy(self.get_all())
+    def get_members_with_posts(self, all_posts):
+        all_members = copy.deepcopy(self.get_all())
 
-		for member, post in itertools.product(all_members, all_posts):
-			if member.id == post.member_id:
-				member.posts.append(post)
+        for member, post in itertools.product(all_members, all_posts):
+            if member.id == post.member_id:
+                member.posts.append(post)
 
-		for member in all_members:
-			yield member
-
+        for member in all_members:
+            yield member
     def get_top_two(self, all_posts):
         all_members = self.get_members_with_posts(all_posts)
 
@@ -71,36 +70,36 @@ class MemberStore:
 
 
 class PostStore:
-	posts = []
+    posts = []
 
-	def add(self, post):
-		PostStore.posts.append(post)
+    def add(self, post):
+        PostStore.posts.append(post)
 
-	def get_all(self):
-		for post in PostStore.posts:
-			print(post)
+    def get_all(self):
+        for post in PostStore.posts:
+            print(post)
 
-	def get_by_id(self, id):
-		all_posts = self.get_all()
-		for post in all_posts:
-			if post.id == id:
-				return post
-		return None
+    def get_by_id(self, id):
+        all_posts = self.get_all()
+        for post in all_posts:
+            if post.id == id:
+                return post
+        return None
 
-	def entity_exists(self, post):
-		result = True
-		if post not in PostStore.posts:
-			result = False
-		return result
+    def entity_exists(self, post):
+        result = True
+        if post not in PostStore.posts:
+            result = False
+        return result
 
-	def delete(self, id):
-		post = self.get_by_id(id)
-		if post:
-			PostStore.posts.remove(post)
+    def delete(self, id):
+        post = self.get_by_id(id)
+        if post:
+            PostStore.posts.remove(post)
 
-	def update(self, post):
-		all_posts = self.get_all()
-		for index, current_post in enumerate(all_members):
-			if post.id == current_post.id:
-				PostStore.posts[index] = post
-				break
+    def update(self, post):
+        all_posts = self.get_all()
+        for index, current_post in enumerate(all_members):
+            if post.id == current_post.id:
+                PostStore.posts[index] = post
+                break
