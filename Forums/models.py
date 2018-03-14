82f7f1ac23a08f3052b004afcb10081ddba84ef1
Hassan Zekkouri <hassanzekkouri@gmail.com>
2018-03-14 01:01:33 +0000

    solution models

--- Forums/models.py
+++ Forums/models.py
@@ -1,21 +1,27 @@
 """
 Project entities!!!!
 """
-class Member:
-	def __init__(self, name, age):
-		self.name = name
-		self.age = age
+class Member():
 
-	def __str__(self):
-		return ("Name: {}\t\tAge: {}".format(self.name, self.age))
+    def __init__(self, name, age):
+        self.id = 0
+        self.name = name
+        self.age = age
+        self.posts = []
+
+    def __str__(self):
+        return f"Name: {self.name}, Age: {self.age}"
 
 
-class Post:
-	def __init__(self, title, body):
-		self.title = title
-		self.body = body
+class Post():
 
-	def __str__(self):
-		return ("Title: {}\nBody: {}\n".format(self.title, self.body))
+    def __init__(self, title, content, member_id=0):
+        self.id = 0
+        self.title = title
+        self.content = content
+        self.member_id = member_id
+
+    def __str__(self):
+        return f"Title: {self.title}, Content: {self.content}"
 
 
